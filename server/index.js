ac187adcded2fa49bc4b497d41244172353d3be9
Joevan Pramana Achmad <116768440+Joevan29@users.noreply.github.com>
2025-06-16 20:58:00 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,20 +6,27 @@ const pool = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173"
+}));
 app.use(express.json());
 
+// Pendaftaran Semua Rute
 const authRoutes = require('./routes/authRoutes');
 const subscriptionRoutes = require('./routes/subscriptionRoutes');
-const adminRoutes = require('./routes/adminRoutes');
 const testimonialRoutes = require('./routes/testimonialRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/subscriptions', subscriptionRoutes);
-app.use('/api/admin', adminRoutes);
 app.use('/api/testimonials', testimonialRoutes);
+app.use('/api/admin', adminRoutes);
 
+// Rute dasar untuk tes
+app.get('/', (req, res) => {
+  res.send('API SEA Catering Berjalan dengan Sukses!');
+});
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
+  console.log(`🚀 Server berjalan di port ${PORT}`);
 });
