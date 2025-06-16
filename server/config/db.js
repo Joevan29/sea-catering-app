1f77b4b195040254b06fa080ecdef633f17a419b
Joevan Pramana Achmad <116768440+Joevan29@users.noreply.github.com>
2025-06-16 16:21:02 +0700

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -6,10 +6,10 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT, 
+  port: process.env.DB_PORT,
+  family: 4,
 });
 
-// Tes koneksi
 pool.connect((err) => {
   if (err) {
     console.error('Koneksi ke database gagal!', err.stack);
